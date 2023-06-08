4e272e1deb12fcc42b0447a39a4c72c4b40395c2
temcenkovolo <temchenkovolo@gmail.com>
2023-06-08 14:43:29 +0200

    added new ShowRestaurants component,some refactor

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import './App.css';
 import {Route, Routes} from "react-router";
 import Layout from "./router/Layout";
 import HomePage from "./Pages/HomePage/HomePage";
-import McDonaldsSection from "./Components/McDonalds/McDonaldsSection";
-import McDaysSection from "./Components/McDays/McDaysSection";
-import KebabSection from "./Components/Kebab/KebabSection";
-import KfcSection from "./Components/Kfc/KfcSection";
 import CartPage from "./Pages/CartPage/CartPage";
+import ShowRestaurants from "./Components/ShowRestaurants/ShowRestaurants";
 
 function App() {
   return (
     <Routes>
     <Route path='/' element={<Layout/>}>
       <Route path="/" element={<HomePage/>}/>
-      <Route path="mcdonalds" element={<McDonaldsSection/>}/>
-      <Route path="kfc" element={<KfcSection/>}/>
-      <Route path="kebab" element={<KebabSection/>}/>
-      <Route path="mcdays" element={<McDaysSection/>}/>
-      <Route path="cart" element={<CartPage/>}/>
+      <Route path="/:showRestaurants" element={<ShowRestaurants/>}/>
+      <Route path="/cart" element={<CartPage/>}/>
     </Route>
     </Routes>
   );
---
+++ src/Components/ShowRestaurants/ShowRestaurants.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {cartAC} from "../Cart/CartAC";
+import {totalPriceAC} from "../../Pages/CartPage/cartPageAC";
+import {useParams} from "react-router";
+
+
+const ShowRestaurants = () => {
+
+    let param = useParams()
+    console.log(param)
+
+    let restaurantData = useSelector((state) => state.restaurants);
+    if (param.showRestaurants === "mcdonalds") {
+        restaurantData = restaurantData.mcDonalds;
+    } else if (param.showRestaurants === "kfc") {
+        restaurantData = restaurantData.kfc;
+    } else if (param.showRestaurants === "kebab") {
+        restaurantData = restaurantData.kebab;
+    } else if (param.showRestaurants === "mcdays") {
+        restaurantData = restaurantData.mcdays;
+
+    }
+
+    let dispatch = useDispatch()
+    const addToCart = (e) => {
+        let getParseData;
+        let getData = localStorage.getItem('product');
+        if (getData) {
+            getParseData = JSON.parse(getData)
+        } else {
+            getParseData = []
+        }
+
+        for (let el of restaurantData) {
+            if (el.id === Number(e.target.id)) {
+                let index = getParseData.findIndex(item => item.id === Number(e.target.id))
+                if (index === -1) {
+                    getParseData.push(el)
+                    dispatch(cartAC(getParseData))
+                    dispatch(totalPriceAC(el.price))
+                } else {
+                    getParseData.splice(index, 1)
+                    dispatch(cartAC(getParseData))
+                }
+
+            }
+        }
+
+        localStorage.setItem('product', JSON.stringify(getParseData))
+    }
+console.log(restaurantData)
+    return (
+        <div>
+            {restaurantData && restaurantData.map(item => <div>
+                <img src={item.image} alt="image-product"/>
+                <h3>{item.product}</h3>
+                <p>Price of product {item.price}$</p>
+                <button id={`${item.id}`} onClick={(e) => addToCart(e)}>add to cart</button>
+            </div>)}
+        </div>
+    );
+};
+
+export default ShowRestaurants;
